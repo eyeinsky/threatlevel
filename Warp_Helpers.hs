a43b7f38eafbe6e919240efad83280159dbf8a3d
Markus Läll <markus.l2ll@gmail.com>
2016-01-10 20:04:35 +0200

    Use own port newtype in myRun, add BaseURL newtype
    BaseURL is used 'toPayload'ed as 'runSite' argument.

--- Warp_Helpers.hs
+++ Warp_Helpers.hs
@@ -7,10 +7,23 @@ import qualified Data.ByteString as B
 import Network.Wai -- (Response, Request, queryString, requestHeaders, pathInfo, requestBody)
 import Network.Wai.Handler.Warp -- (Response, Request, queryString, requestHeaders, pathInfo, requestBody)
 
+import HTTP_Common
+import HTTP_URL as U
+
 getRequestBody req = BL.fromChunks <$> loop
    where
       loop = requestBody req >>= re
       re chunk = B.null chunk ? return [""] $ (chunk:) <$> loop
 
-myRun :: Int -> (Request -> IO Response) -> IO ()
-myRun port f = runSettings (setPort port $ defaultSettings) $ \ r r' -> r' =<< f r
+myRun :: U.Port -> (Request -> IO Response) -> IO ()
+myRun (U.Port port) f
+   = runSettings (setPort (fromIntegral port) $ defaultSettings) $ \ r r' -> r' =<< f r
+
+newtype BaseURL = BaseURL (U.Proto, U.Host, U.Port)
+instance ToPayload BaseURL where
+   toPayload (BaseURL (proto, host, port)) =
+         toPayload proto
+      <> protoSep
+      <> toPayload host
+      <> portSep
+      <> toPayload port
