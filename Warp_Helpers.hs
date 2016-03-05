2adb898031f14f28116c843f20407d687980025d
Markus Läll <markus.l2ll@gmail.com>
2016-03-05 23:22:08 +0100

    Create 'runDomains' for running virtual hosts

--- Warp_Helpers.hs
+++ Warp_Helpers.hs
@@ -3,9 +3,12 @@ module Warp_Helpers where
 
 import Prelude2 as P
 import qualified Data.ByteString.Lazy as BL
+import qualified Data.Text.Lazy.Encoding as TLE
+import qualified Data.Text.Lazy as TL
 import qualified Data.ByteString as B
 import Network.Wai -- (Response, Request, queryString, requestHeaders, pathInfo, requestBody)
 import Network.Wai.Handler.Warp -- (Response, Request, queryString, requestHeaders, pathInfo, requestBody)
+import Network.HTTP.Types
 
 import HTTP_Common
 import HTTP_URL as U
@@ -18,6 +21,25 @@ getRequestBody req = BL.fromChunks <$> loop
 myRun :: U.Port -> (Request -> IO Response) -> IO ()
 myRun (U.Port port) f
    = runSettings (setPort (fromIntegral port) $ defaultSettings) $ \ r r' -> r' =<< f r
+
+runDomains :: U.Port -> [(Maybe U.Authority, IO (Request -> IO Response))] -> IO ()
+runDomains (U.Port port) sites
+   = do
+      -- Listening on: mapM (print . fst) sites
+      pairs' <- forM sites $ \ (a, b) -> (toHost <$> a,) <$> b
+      runSettings (setPort (fromIntegral port) $ defaultSettings) $
+         \ req respond -> let
+               domain = getDomain req :: Maybe B.ByteString
+               resp = snd <$> find ((== domain) . fst) pairs' :: Maybe (Request -> IO Response)
+            in do
+            respond =<< fromMaybe noDomain (($ req) <$> resp)
+   where
+      getDomain = lookup "Host" . requestHeaders
+      noDomain = return $ responseLBS status404 [] "No host"
+      toHost authority = authority { U.authentication = Nothing }
+         & toPayload
+         & TLE.encodeUtf8
+         & BL.toStrict
 
 newtype BaseURL = BaseURL (U.Proto, U.Host, U.Port)
 instance ToPayload BaseURL where
