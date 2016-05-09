dcbf3102b66c6127d66781da4467506bdfc929af
Markus Läll <markus.l2ll@gmail.com>
2016-05-09 22:52:46 +0200

    Add cls_ helper to use classes directly in blaze

--- Web/Blaze.hs
+++ Web/Blaze.hs
@@ -1,7 +1,12 @@
 module Web.Blaze where
 
+import Prelude
+import qualified Data.Text.Lazy as TL
+
 import qualified Text.Blaze.Html5.Attributes as A
 import qualified Text.Blaze.Html5            as E
+
+import qualified Web_CSS as CSS
 
 jsTag = E.script E.! A.type_ "text/javascript"
 cssTag = E.style E.! A.type_ "text/css"
@@ -9,3 +14,5 @@ favicon adr = E.link
    E.! A.rel "shortcut icon"
    E.! A.type_ "image/x-icon"
    E.! A.href (E.toValue adr)
+
+cls_ strs = A.class_ $ E.toValue $ TL.unwords $ map CSS.unClass strs
