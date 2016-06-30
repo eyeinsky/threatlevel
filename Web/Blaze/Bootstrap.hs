86a7d69c9f8e8146ea1aba5ae7e37991adac41f9
Markus Läll <markus.l2ll@gmail.com>
2016-06-30 12:30:03 +0300

    Improve Web.Blaze.Bootstrap

--- Web/Blaze/Bootstrap.hs
+++ Web/Blaze/Bootstrap.hs
@@ -1,34 +1,33 @@
-module Web.Blaze.Bootstrap
-  (css, themeCss, js) where
+module Web.Blaze.Bootstrap where
 
 import Prelude2
 import qualified Text.Blaze.Html5.Attributes as A
 import qualified Text.Blaze.Html5            as E
 
+import Web.Blaze
+
 crossorigin = E.customAttribute "crossorigin"
 integrity = E.customAttribute "integrity"
-(!) = (E.!)
 
 -- Latest compiled and minified CSS
-css = do
-  E.link
-    ! A.rel "stylesheet"
-    ! A.href "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
-    ! integrity "sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
-    ! crossorigin "anonymous"
+css = cssUrl "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+  E.! integrity "sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
+  E.! crossorigin "anonymous"
 
 -- Optional theme
-themeCss = do
-  E.link
-    ! A.rel "stylesheet"
-    ! A.href "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
-    ! integrity "sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r"
-    ! crossorigin "anonymous"
+themeCss = cssUrl "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
+  E.! integrity "sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r"
+  E.! crossorigin "anonymous"
 
 -- Latest compiled and minified JavaScript
 js = do
-  E.script
-    ! A.src "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
-    ! integrity "sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
-    ! crossorigin "anonymous"
-    $ ""
+  jsUrl "https://code.jquery.com/jquery-2.2.4.min.js"
+    E.! integrity "sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+    E.! crossorigin "anonymous"
+  jsUrl "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+    E.! integrity "sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+    E.! crossorigin "anonymous"
+
+
+container = E.div E.! cls_ ["container"]
+row = E.div E.! cls_ ["row"]
