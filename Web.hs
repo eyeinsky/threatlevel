ef4bfd41b005b7cccc0f77fb5d18b4aa6f5e09c7
Markus Läll <markus.l2ll@gmail.com>
2016-12-20 15:23:25 +0100

    Export Web.Monad from Web

--- Web.hs
+++ Web.hs
@@ -2,8 +2,10 @@ module Web
    ( module Web.CSS
    , module Web.HTML.Core
    , module Web.Browser
+   , module Web.Monad
    ) where
 
 import Web.CSS
 import Web.HTML.Core
 import Web.Browser
+import Web.Monad
