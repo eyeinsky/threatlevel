af5ebe07c9730dba30b74a60070f1e0617484d0d
Markus Läll <markus.l2ll@gmail.com>
2017-01-24 16:31:02 +0100

    fixup! Adapt to the Render class now using lazy text

--- JS/Blaze.hs
+++ JS/Blaze.hs
@@ -4,16 +4,15 @@ import Prelude2
 import qualified Text.Blaze.Html5 as E
 import JS
 
-_ASTtoText = runRender . renderM
 instance E.ToValue (Expr a) where
-   toValue = E.toValue . _ASTtoText
-   preEscapedToValue = E.preEscapedToValue . _ASTtoText
+   toValue = E.toValue . render
+   preEscapedToValue = E.preEscapedToValue . render
 instance E.ToValue (Code a) where
-   toValue = E.toValue . _ASTtoText
-   preEscapedToValue = E.preEscapedToValue . _ASTtoText
+   toValue = E.toValue . render
+   preEscapedToValue = E.preEscapedToValue . render
 instance E.ToMarkup (Expr a) where
-   toMarkup = E.toMarkup . _ASTtoText
-   preEscapedToMarkup = E.preEscapedToMarkup . _ASTtoText
+   toMarkup = E.toMarkup . render
+   preEscapedToMarkup = E.preEscapedToMarkup . render
 instance E.ToMarkup (Code a) where
-   toMarkup = E.toMarkup . _ASTtoText
-   preEscapedToMarkup = E.preEscapedToMarkup . _ASTtoText
+   toMarkup = E.toMarkup . render
+   preEscapedToMarkup = E.preEscapedToMarkup . render
