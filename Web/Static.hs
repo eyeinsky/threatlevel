a21aca1fed104d4600db1d77667afddd0cc3c057
Markus Läll <markus.l2ll@gmail.com>
2017-05-01 23:04:18 +0200

    Add Web.Static for static sites of own monad

---
+++ Web/Static.hs
@@ -0,0 +1,38 @@
+module Web.Static where
+
+import Prelude2 hiding (head, text)
+import qualified Data.Text.Lazy as TL
+
+import JS
+import Web hiding (content)
+import Web.HTML
+
+import Web.Monad (run)
+import Web.Browser
+
+runStatic :: WebT Identity (HTMLM ()) -> HTMLM ()
+runStatic wm = html $ do
+  head $ do
+    cssTag $ renderRaw $ wr^.cssCode <> reset
+    jsTag $ renderRaw $ wr^.jsCode
+    favicon "data:;base64,iVBORw0KGgo="
+    meta ! httpEquiv "Content-Type" ! content "text/html; charset=utf-8" $ pure ()
+  body
+  where
+    browser = Unknown
+    (body :: HTMLM (), _, wr) = runIdentity $ run browser wm
+    reset = setBoxSizing browser <> resetCSS browser
+
+
+cssTag :: HTMLM () -> HTMLM ()
+cssTag = style ! type_ "text/css"
+
+jsTag :: HTMLM () -> HTMLM ()
+jsTag = script ! type_ "text/javascript"
+
+favicon :: TL.Text -> HTMLM ()
+favicon adr = link
+  ! rel "shortcut icon"
+  ! type_ "image/x-icon"
+  ! href adr
+  $ pure ()
