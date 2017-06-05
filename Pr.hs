f4eb23f5e9318e6ae71eceb7816e33705a0cb765
Markus Läll <markus.l2ll@gmail.com>
2017-06-05 10:05:55 +0200

    Export standard monads from Pr

--- Pr.hs
+++ Pr.hs
@@ -1,6 +1,19 @@
 module Pr
   ( module Pr
   , module Prelude2
+  , module Control.Monad
+  , module Control.Monad.Except
+  , module Control.Monad.Reader
+  , module Control.Monad.Writer
+  , module Control.Monad.State
+  , module Control.Monad.RWS
   ) where
 
 import Prelude2
+
+import Control.Monad
+import Control.Monad.Except
+import Control.Monad.Reader
+import Control.Monad.Writer
+import Control.Monad.State
+import Control.Monad.RWS (RWST(..), runRWST)
