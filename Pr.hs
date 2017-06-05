130c079015a781c02d6e4c06f0e24700de7c64ce
Markus Läll <markus.l2ll@gmail.com>
2017-06-05 18:09:46 +0200

    Export text/bytestring lens from Pr; hide Writer and State

--- Pr.hs
+++ Pr.hs
@@ -1,6 +1,9 @@
 module Pr
   ( module Pr
   , module Prelude2
+  , module Data.Default
+  , module Data.ByteString.Lens
+  , module Data.Text.Lazy.Lens
   , module Control.Monad
   , module Control.Monad.Except
   , module Control.Monad.Reader
@@ -10,10 +13,13 @@ module Pr
   ) where
 
 import Prelude2
+import Data.Default
+import Data.ByteString.Lens
+import Data.Text.Lazy.Lens hiding (Text, _Text, packed, builder, text, unpacked)
 
 import Control.Monad
 import Control.Monad.Except
-import Control.Monad.Reader
-import Control.Monad.Writer
-import Control.Monad.State
+import Control.Monad.Reader hiding (Reader)
+import Control.Monad.Writer hiding (Writer)
+import Control.Monad.State  hiding (State)
 import Control.Monad.RWS (RWST(..), runRWST)
