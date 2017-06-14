4fda6f98f5ae0e61a3f06259a8377298e9d118e5
Markus Läll <markus.l2ll@gmail.com>
2017-06-14 08:48:27 +0200

    Add on for use as attribute

--- DOM.hs
+++ DOM.hs
@@ -1,7 +1,14 @@
 module DOM
   ( module DOM.Event
   , module DOM.JS
+  , on
   ) where
 
+import Pr hiding (on)
 import DOM.Event
 import DOM.JS
+import HTML
+import JS
+
+on :: Event a => a -> Expr a1 -> Attribute
+on event js = Custom (toOn event) (render $ call1 js (ex "event"))
