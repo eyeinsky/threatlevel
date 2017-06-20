959cbdc46a41fc68c1e50a7f635d3ed9df745e8c
Markus Läll <markus.l2ll@gmail.com>
2017-06-20 19:47:07 +0200

    Add Web.CSS for resets

--- Web.hs
+++ Web.hs
@@ -1,5 +1,6 @@
 module Web
    ( module CSS
+   , module Web.CSS
    , module HTML
    , module Web.Browser
    , module Web.Monad
@@ -7,6 +8,7 @@ module Web
    ) where
 
 import CSS hiding (run, (!), M, id)
+import Web.CSS (reset)
 import HTML hiding (Value, content, font, em, id)
 import Web.Browser
 import Web.Monad
---
+++ Web/CSS.hs
@@ -0,0 +1,16 @@
+module Web.CSS where
+
+import Pr
+import HTML (TagName(TagName))
+import Web.Monad
+import CSS
+
+reset = do
+  cssRule (TagName "body") zero
+  cssRule (TagName "div") zero
+  cssRule (TagName "html") (boxSizing "border-box")
+  cssRule anyTag $ boxSizing "inherit"
+  where
+    zero = do
+      padding $ px 0
+      margin $ px 0
