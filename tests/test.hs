fc96bac581679aaf6dfab792ad98c8b9129580e3
Markus Läll <markus.l2ll@gmail.com>
2017-07-16 11:55:06 +0200

    Make the tests compile
    It now doesn't do anyhting since there is no typed js as of now.

--- tests/test.hs
+++ tests/test.hs
@@ -7,6 +7,8 @@ main = putStr "Tests!"
 
 test = let
    in do
+   putStr "Tests!"
+   {-
    return1 :: Expr (Expr JS.String, Proxy JS.NumberI)
       <- newf' "ret1" $ \ (s :: Expr JS.String) ->
          retrn (lit (1::Int) :: Expr JS.NumberI)
@@ -16,3 +18,4 @@ test = let
    bare $ addArgs `a2` (lit (2::Int), lit (3::Int))
    -- bare $ addArgs -/ lit ("string" :: String) -/ lit (3::Int)     -- errors due to wrong type of argument
    -- bare $ addArgs -/ lit (2::Int) -/ lit (3::Int) -/ lit (4::Int) -- errors due to type mismatch (caused by too many arguments)
+-}
