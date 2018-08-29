4480bd7cf1100673c737cfe21d5e453e7343480a
Markus Läll <markus.l2ll@gmail.com>
2018-08-29 11:22:12 +0200

    Add deleteCookie to DOM.JS

--- DOM/JS.hs
+++ DOM/JS.hs
@@ -359,3 +359,7 @@ putOnload :: Code a -> Code b
 putOnload code = [BareExpr $ onload =: func]
   where
     func = AnonFunc Nothing [] code :: Expr b
+
+deleteCookie name = do
+  document !. "cookie" .= value
+  where value = name .+ "=; expires=Thu, 01 Jan 1970 00:00:01 GMT;"
