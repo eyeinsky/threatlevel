62e6f3c6db7a8ba61d38eb1296e3c7531d24f3f6
Markus Läll <markus.l2ll@gmail.com>
2018-08-29 10:28:16 +0200

    Add cookie handling http responses to X

--- X.hs
+++ X.hs
@@ -17,3 +17,24 @@ on event handler = Custom (DOM.toOn event) (render def $ call1 handler $ ex "eve
     -- JS.Syntax.EName (JS.Syntax.Name handler') = handler
     -- ^ todo: find a generic way to get the name, even for literal
     -- expressinos.
+
+-- * HTML
+
+href :: URL.URL -> Attribute
+href url = HTML.href (WR.renderURL url)
+
+-- * HTTP.Response
+
+-- todo: make this a lens for headers on AnyResponse
+withHeaders f req = case req of
+  WR.Raw s h b -> WR.Raw s (f  h) b
+  _ -> req
+
+deleteCookie :: TS.Text -> WR.AnyResponse -> WR.AnyResponse
+deleteCookie key = withHeaders (Hdr.delC (TL.fromStrict key) :)
+
+setCookie :: TS.Text -> TS.Text -> WR.AnyResponse -> WR.AnyResponse
+setCookie k v = withHeaders (setCookie (TL.fromStrict v))
+  where
+    setCookie :: TL.Text -> [Hdr.Header] -> [Hdr.Header]
+    setCookie val = (Hdr.cookie' (TL.fromStrict k) val Nothing (Just []) Nothing :)
