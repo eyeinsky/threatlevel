4f437151acc49fb18c429622ba735d3fc6cdcfb9
Markus Läll <markus.l2ll@gmail.com>
2019-05-10 08:57:11 +0200

    Fix up the HTTP module [master]

--- web/HTTP.hs
+++ web/HTTP.hs
@@ -1,26 +1,8 @@
-{-
-TODO
-   * implement POST-ing, postForm
-   * convert to lens
-   * Payload is now BS, but ToPayload renders to T (lazy text)
-      - need to look what headers are (ASCII)
-      - body can definitely be BS
--}
 module HTTP
-   (
-     module HTTP.Header
-   , module HTTP.Netw
-   , module HTTP.Request
+   ( module HTTP.Header
    , module HTTP.Response
-   , module HTTP.Util
-   , module URL
    )
    where
 
-
-import HTTP.Header
-import HTTP.Netw
-import HTTP.Request
+import HTTP.Header hiding (Custom)
 import HTTP.Response
-import HTTP.Util
-import URL.ToPayload as URL
