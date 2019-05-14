34b30ac200010b82633ad7fc99c4ff916b90408a
Markus Läll <markus.l2ll@gmail.com>
2019-05-14 14:20:05 +0200

    Add readCookie helper to X.Wai [master]

--- web/X/Wai.hs
+++ web/X/Wai.hs
@@ -1,8 +1,15 @@
 module X.Wai where
 
+import Control.Monad
+import qualified Data.Text as TS
 import Network.Wai
 import Network.HTTP.Types
+import Web.Cookie (parseCookiesText)
 
 import X.Prelude
 
+queryText :: Request -> QueryText
 queryText = queryString ^ queryToQueryText
+
+readCookie :: TS.Text -> Request -> Maybe TS.Text
+readCookie key = requestHeaders ^ lookup hCookie >=> parseCookiesText ^ lookup key
