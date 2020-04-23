913c6dd3a373043223eb3c4140cf34a52159499c
eyeinsky <eyeinsky9@gmail.com>
2020-04-23 11:21:19 +0300

    Add mkPromise helper [master]

--- web/JS/BuiltIns/Promise.hs
+++ web/JS/BuiltIns/Promise.hs
@@ -6,3 +6,14 @@ import JS.DSL
 newPromise executor =  call1 (New $ ex "Promise") executor
 
 reject = ex "Promise" !// "reject"
+
+-- * Util
+
+-- | Create a promise and return it together with the function to
+-- resolve it.
+mkPromise :: M r (Expr f, Expr p)
+mkPromise = do
+  resolve <- let_ Null
+  executor <- newf $ \resolve' -> resolve .= resolve'
+  promise <- const $ newPromise executor
+  return (resolve, promise)
