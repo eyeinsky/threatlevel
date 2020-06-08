ced9b5181918fe144741ca1d4dc88a91a721e513
eyeinsky <eyeinsky9@gmail.com>
2020-06-08 10:14:10 +0300

    Add reject to mkPromise [master]

--- web/JS/BuiltIns/Promise.hs
+++ web/JS/BuiltIns/Promise.hs
@@ -9,11 +9,14 @@ reject = ex "Promise" !// "reject"
 
 -- * Util
 
--- | Create a promise and return it together with the function to
--- resolve it.
-mkPromise :: M r (Expr f, Expr p)
+-- | Create a promise and return it together with the functions to
+-- resolve and reject it.
+mkPromise :: M r (Expr resolve, Expr reject, Expr p)
 mkPromise = do
   resolve <- let_ Null
-  executor <- newf $ \resolve' -> resolve .= resolve'
+  reject <- let_ Null
+  executor <- newf $ \resolve' reject' -> do
+    resolve .= resolve'
+    reject .= reject'
   promise <- const $ newPromise executor
-  return (resolve, promise)
+  return (resolve, reject, promise)
