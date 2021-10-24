77e36bea74db0d094fb258d1f30b4ac4af5338b6
Markus Läll <markus.l2ll@gmail.com>
2021-10-24 14:49:24 +0300

    Add distinct Get and Init to GetTemplate and Template
    .. also remove `templateSsr` and change `Template'`.

--- threatlevel-web/X/Template/V3.hs
+++ threatlevel-web/X/Template/V3.hs
@@ -1,35 +1,32 @@
 {-# LANGUAGE RecordWildCards #-}
 module X.Template.V3
   ( module X.Template.V3
-  , module X.Template.V3.Common
+  , module X.Template.Common
   ) where
 
 import X.Prelude
 import X
-import X.Template.V3.Common
+import X.Template.Common
+import JS.DSL.MTL.Function
+
 
 -- * API
 
-data SSR a out = SSR
-  { sSRfields :: Slots
-  , sSRSsr :: a -> Html
-  , sSROut :: out
-  }
-makeFields ''SSR
+type Creates a = Expr a -> Expr (Context a)
+type Updates a = Expr a -> Expr (Context a) -> Expr ()
+type Gets a = Expr [Node] -> Expr a
+type Slots = [Class]
 
-data Template a out = Template
+data Template a out get = Template
   { templateSlots :: Slots
   , templateCreate :: Creates a
   , templateUpdate :: Updates a
-  , templateGet :: Gets a
-
-  -- | Both create and ssr map a to the input of html
-  , templateSsr :: a -> Html -- ssr
-
+  , templateGet :: Gets get
   , templateOut :: out
   }
 makeFields ''Template
 
+type Template' a = Template a (Out a) (Get a)
 
 class GetTemplate t where
   type In t :: *
@@ -39,4 +36,42 @@ class GetTemplate t where
   type Out t :: *
   type Out t = ()
 
-  getTemplate :: (Monad m, MonadFix m) => In t -> WebT m (Template t (Out t))
+  type Get t :: *
+  type Get t = t
+
+  getTemplate
+    :: (Monad m, MonadFix m)
+    => In t -> WebT m (Template t (Out t) (Get t))
+
+
+-- * Helpers
+
+-- ** Slots
+
+nSlots :: forall m. MonadWeb m => Int -> m Slots
+nSlots n = replicateM n (css $ pure ())
+
+-- | Create classes and all field getters
+nSlotGetters :: MonadWeb m => Int -> m (Slots, [Gets Tag])
+nSlotGetters n = do
+  fields <- nSlots n
+  getters <- mapM mkGet fields
+  return (fields, getters)
+
+-- ** Create, update, get
+
+nCreate :: MonadWeb m => m (Creates a)
+nCreate = js $ fn $ \o -> do
+  throw "templateCreate not implemented"
+  ctx <- createContext o $ "temlpateCreate not implemented"
+  return_ ctx
+
+nUpdate :: MonadWeb m => m (Updates a)
+nUpdate = js $ fn $ \(_ :: Expr a) (_ :: Expr (Context a)) -> do
+  throw "templateUpdate not implemented"
+  return_ (Undefined :: Expr ())
+
+nGet :: forall a m. (MonadWeb m, Back (Expr a), Convert (Expr a) ~ Expr a) => m (Gets a)
+nGet = js $ fn $ \(_ :: Expr [Node]) -> do
+  throw "templateGet not implemented"
+  return_ (Undefined :: Expr a)
