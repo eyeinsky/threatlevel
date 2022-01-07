1beebed26abb8615c8818eb0d7f73d2e32a245c4
Markus Läll <markus.l2ll@gmail.com>
2022-01-07 17:26:37 +0200

    [js] Compiling but probably insufficient

--- threatlevel-js/JS/DSL/Polysemy/Core.hs
+++ threatlevel-js/JS/DSL/Polysemy/Core.hs
@@ -8,12 +8,10 @@ module JS.DSL.Polysemy.Core
   ) where
 
 import Common.Prelude hiding (next)
-import X.Prelude ((&), Identity(..))
 import qualified Data.Text as TS
 import qualified Data.HashMap.Strict as HS
 
 import Polysemy hiding (run)
-import qualified Polysemy
 import Polysemy.State
 import Polysemy.Reader
 import Polysemy.Writer
@@ -22,10 +20,8 @@ import Polysemy.Fixpoint
 import JS.Syntax as Syntax
 import JS.DSL.Core hiding (State)
 import qualified JS.DSL.Core as Core
-import Identifiers hiding (next)
 
-
-type List' e =
+type Base e =
   [ Writer (Code e)
   , State Idents
   , State Used
@@ -34,26 +30,77 @@ type List' e =
   , Fixpoint
   , Final Identity
   ]
-type List = List' ()
 
-type BasePoly r = Members List r
-type BaseMono a = Sem List a
+type BaseMono e {-a-} = Sem (Base e) {-a-}
+type BaseResult r a = (Lib, (Used, (Idents, (Code r, a))))
+
+-- type Poly e r a = Members (Base e) r => Sem r a
 
 -- * Effect
 
-data Emit :: Effect where
-  GetIdentifier :: Emit m Name
-  EmitStatement :: Statement () -> Emit m ()
-  -- EmitFunctionDefinition
-  --   :: BasePoly r => Name -> Sem (Emit : r) () -> Emit m ()
+data JS r e m a where
+  GetFreshIdentifier :: JS r e m Name
+  GetPrefixedIdentifier :: TS.Text -> JS r e m Name
+  EmitStatement :: Statement e -> JS r e m ()
+  DefineFunction :: (Base _e ~ r) => Sem (JS r _e : r) _a -> JS r e m (Code _e)
+  -- ^ @s@ is the monomorphic rest of the effects under JS that need
+  -- to contain Base. It's required because otherwise from final type
+  -- @JS e m ()@ (the @s@ omitted) we wouldn't know what the sub-row
+  -- was.
 
-makeSem ''Emit
+makeSem ''JS
 
 -- * Run
 
-runBase
-  :: Syntax.Conf -> Lib -> Used -> Idents
-  -> BaseMono a -> MonoResult () a
+-- | Interpret JS in terms of Base
+jsToBase
+  :: forall r e a . Members (Base e) r
+  => (forall _e _a . RunMono _e _a)
+  -- ^ run sub-JS to syntax syntax
+  -> Sem (JS r e : r) a -> Sem r a
+jsToBase f = interpret $ \case
+  GetFreshIdentifier -> do
+    Infinite x xs <- get
+    put xs
+    return $ Name x
+  GetPrefixedIdentifier name -> do
+    names :: Used <- get
+    case HS.lookup name names of
+      Just (Infinite s uffixes) -> do
+        modify (HS.insert name uffixes)
+        return $ Name $ name <> "_" <> s
+      _ -> do
+        let suffixes = fmap TS.pack $ bigEndian ['a' .. 'z']
+        modify @Used (HS.insert name suffixes)
+        return $ Name name
+  EmitStatement stm -> tell (pure stm :: Code e)
+  DefineFunction (body :: Sem (JS r _e : r) _a) -> do
+    env :: Syntax.Conf <- ask
+    fresh0 :: Idents <- get
+    used0 :: Used <- get
+    lib0 :: Lib <- get
+    let
+      f' :: Sem (JS r _e : r) _a -> BaseResult _e _a
+      f' = f env lib0 used0 fresh0
+      (lib1, (used1, (fresh1, (code, a)))) = f' body
+    put @Idents fresh1
+    put @Used used1
+    put @Lib lib1
+    return code
+
+-- | Anything that can run monomorphic base
+type RunBase a = Syntax.Conf -> Lib -> Used -> Idents -> a
+-- | Run monomorphic JS to result
+type RunMono e a = RunBase (Sem (JS (Base e) e : (Base e)) a -> BaseResult e a)
+
+-- | Interpret @JS e@ in terms of @Base e@
+runMono :: forall e a . RunMono e a
+runMono env lib used fresh m = m
+  & jsToBase runMono
+  & runBase env lib used fresh
+
+-- | Run monomorphic
+runBase  :: RunBase (Sem (Base e) a -> (BaseResult e a))
 runBase env lib used fresh m = m
   & runWriter
   & runState fresh
@@ -64,123 +111,20 @@ runBase env lib used fresh m = m
   & runFinal
   & runIdentity
 
-runAsBase
-  :: forall r a
-   . BasePoly r
-  => Syntax.Conf -> Lib -> Used -> Idents -> Sem (Emit : r) a -> Sem r a
-runAsBase env lib used idents = interpret $ \case
-  GetIdentifier -> do
-    Infinite x xs <- get
-    put xs
-    return $ Name x
-  EmitStatement stm -> tell (pure stm :: Code ())
---   EmitFunctionDefinition name body -> do
--- --    let z = runEmit env lib used idents body :: Sem m ()
---     undefined
---   _ -> undefined
+-- * Old API defined in terms of JS
 
--- runWithBase ::
--- runWithBase runBase' env lib used idents m = runBase' runAsBase m
+write :: forall r s e . Member (JS s e) r => Statement e -> Sem r ()
+write = emitStatement @s
 
--- * Polysemy
+next :: forall r e . Member (JS r e) r => Sem r Name
+next = getFreshIdentifier @r @e
 
--- | Fixed effect order similar to MTL
-type Mono r = Sem (List' r)
-type MonoResult r a = (Lib, (Used, (Idents, (Code r, a))))
+pushName :: forall r e . Member (JS r e) r => TS.Text -> Sem r Name
+pushName = getPrefixedIdentifier @r @e
 
-type States m =
-  ( State Idents `Member` m
-  , State Used `Member` m
-  , State Lib `Member` m
-  )
-
-type C r m =
-  ( Writer (Code r) `Member` m
-  , States m
-  , Reader Syntax.Conf `Member` m
-  )
-
-type Poly r m a = C r m => Sem m a
-
-runPolysemy :: Syntax.Conf -> Lib -> Used -> Idents -> Mono r a -> MonoResult r a
-runPolysemy env lib used fresh m = m
-  & runWriter
-  & runState fresh
-  & runState used
-  & runState lib
-  & runReader env
-  & fixpointToFinal @Identity
-  & runFinal
-  & runIdentity
-
-polysemy2common :: MonoResult r a -> Result r a
-polysemy2common (lib, (used, (fresh, (code, a)))) =
-  ((a, code), Core.State fresh used lib)
-
--- * API
-
--- ** Fixed
-
-type M r = Mono r
-
-run :: Syntax.Conf -> Lib -> Used -> Idents -> M r a -> Result r a
-run env lib used fresh m = polysemy2common $ runPolysemy env lib used fresh m
-
--- **  Poly
-
-getState :: States m => Sem m Core.State
-getState = do
-  fresh :: Idents <- get
-  used :: Used <- get
-  lib :: Lib <- get
-  return $ Core.State fresh used lib
-
-putState :: States m => Core.State -> Sem m ()
-putState (Core.State fresh used lib) = put fresh *> put used *>  put lib
-
-getLibrary :: (State Lib `Member` m) => Sem m Lib
-getLibrary = get @Lib
-
-modifyLibrary :: (State Lib `Member` m) => (Lib -> Lib) -> Sem m ()
-modifyLibrary = modify
-
-askEnv :: (Reader Syntax.Conf `Member` m) => Sem m Syntax.Conf
-askEnv = ask @Syntax.Conf
-
--- | Write JS
-write :: forall r m. Writer (Code r) `Member` m => Statement r -> Sem m ()
-write stm = tell (pure stm :: Code r)
-
--- | Get next identifier
-next :: State Idents `Member` m => Sem m Name
-next = do
-  Infinite x xs <- get
-  put xs
-  return $ Name x
-
-pushName :: State Used `Member` m => TS.Text -> Sem m Name
-pushName name = do
-  names :: Used <- get
-  case HS.lookup name names of
-    Just (Infinite s uffixes) -> do
-      modify (HS.insert name uffixes)
-      return $ Name $ name <> "_" <> s
-    _ -> do
-      let suffixes = fmap TS.pack $ bigEndian ['a' .. 'z']
-      modify @Used (HS.insert name suffixes)
-      return $ Name name
-
-mkCode :: forall ra rb m a. Mono rb a -> Poly ra m (Code rb)
-mkCode m = do
-  fresh0 :: Idents <- get
-  used0 :: Used <- get
-  lib0 :: Lib <- get
-  env :: Syntax.Conf <- ask
-  let (lib1, (used1, (fresh', (code, _)))) = runPolysemy @rb env lib0 used0 fresh0 m
-  put fresh'
-  put used1
-  put lib1
-  return code
-
-mkCode_ :: forall r m a . Mono r a -> Poly r m (Code r)
-mkCode_ = mkCode @r @r
+runBaseMonoToResult :: Syntax.Conf -> Lib -> Used -> Idents -> Sem (Base r) a -> Result r a
+runBaseMonoToResult env lib used fresh m = polysemy2common $ runBase env lib used fresh m
+  where
+  polysemy2common :: BaseResult r a -> Result r a
+  polysemy2common (lib, (used, (fresh, (code, a)))) =
+    ((a, code), Core.State fresh used lib)
