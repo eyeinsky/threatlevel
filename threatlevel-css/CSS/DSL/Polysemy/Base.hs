5c2002d74c4b25ade48c5c98f57b0a367f4ca181
Markus Läll <markus.l2ll@gmail.com>
2022-01-20 19:27:22 +0200

    [css] Add recursive css monad

--- threatlevel-css/CSS/DSL/Polysemy/Base.hs
+++ threatlevel-css/CSS/DSL/Polysemy/Base.hs
@@ -39,23 +39,44 @@ emitRulesBase rules = tell rules
 emitDeclarationsBase :: Members Base r => Declarations -> Sem r ()
 emitDeclarationsBase ds = tell ds
 
+-- | Run @sub@ to final result with @f@. @f@ is passed in from the
+-- enclosing monad.
+withDerivedSelectorBase
+  :: forall r b a
+   . Members Base b
+  => (forall _a . RunBase (Sem r _a -> BaseResult _a))
+  -> (Selector -> Selector) -> Sem r a -> Sem b ()
+withDerivedSelectorBase f mod sub = do
+  selector <- ask @Selector
+  fresh0 <- get @Names
+  let (fresh1, (ds, (rs, a))) = f selector fresh0 sub :: BaseResult a
+  put @Names fresh1
+  return ()
+
 prop :: Members Base r => TS.Text -> Value -> Sem r ()
 prop k v = emitDeclarationsBase $ pure $ Declaration k v
 
 -- * Interpret @CSS@ to @Base@
 
-cssToBase :: Members Base r => Sem (CSS : r) a -> Sem r a
-cssToBase = interpret $ \case
+cssToBase
+  :: forall r b a
+   . (Members Base b, r ~ (CSS b : b))
+  => (forall _a . RunBase (Sem r _a -> BaseResult _a))
+  -> Sem (CSS b : b) a -> Sem b a
+cssToBase f = interpret $ \case
   GetFreshClass -> Class <$> getFreshClassBase
   EmitRules s ds -> emitRulesBase $ pure $ mkRule s ds
   EmitDeclarations ds -> emitDeclarationsBase ds
+  WithDerivedSelector mod (sub :: Sem r _a) ->
+    withDerivedSelectorBase f mod sub
 
 -- * Monomorphic @Base@
 
+type RunBase a = Selector -> Names -> a
 type BaseResult a = (Names, (Declarations, (Rules, a)))
 
 -- | Run monomorphic @Base@
-runBase :: Selector -> Names -> Sem Base a -> BaseResult a
+runBase :: RunBase (Sem Base a -> BaseResult a)
 runBase r s m = m
   & runWriter @Rules
   & runWriter @Declarations
@@ -64,14 +85,14 @@ runBase r s m = m
   & Polysemy.run
 
 -- | Run monomorphic @CSS@
-run :: Selector -> Names -> Sem (CSS : Base) a -> BaseResult a
+run :: Selector -> Names -> Sem (CSS Base : Base) a -> BaseResult a
 run r s m = m
-  & cssToBase
+  & cssToBase run
   & runBase r s
 
 -- | Run monomorphic @CSS@ from empty start
-runEmpty :: Sem (CSS : Base) a -> BaseResult a
+runEmpty :: Sem (CSS Base : Base) a -> BaseResult a
 runEmpty m = run (selFrom $ Tag "") identifiers m
 
-instance Render (Sem (CSS : Base) a) where
+instance Render (Sem (CSS Base : Base) a) where
   renderM m = undefined
