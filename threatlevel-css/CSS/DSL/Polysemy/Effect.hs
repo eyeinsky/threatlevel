e944a440c0262daf3eff5d4a4b2b4cd90e5c2436
Markus Läll <markus.l2ll@gmail.com>
2022-01-29 22:12:39 +0200

    [css] Move effect code to CSS.DSL.Polysemy.Effect
    .. and remove module CSS.DSL.Polysemy.

--- threatlevel-css/CSS/DSL/Polysemy/Effect.hs
+++ threatlevel-css/CSS/DSL/Polysemy/Effect.hs
@@ -2,6 +2,7 @@
 module CSS.DSL.Polysemy.Effect where
 
 import Common.Prelude
+import Common.Polysemy
 import qualified Data.Text as TS
 
 import Polysemy
@@ -14,6 +15,8 @@ data Prop :: Effect where
 
 makeSem ''Prop
 
+-- * CSS
+
 data CSS (s :: EffectRow) :: Effect where
   GetFreshClass :: CSS s m Class
   GetSelector :: CSS s m Selector
@@ -24,13 +27,43 @@ data CSS (s :: EffectRow) :: Effect where
 
 makeSem ''CSS
 
+-- | Generate a class, bind declarations @m@ to it
+css :: forall s r a . RecEff s r CSS => Sem r a -> Sem r Class
+css m = do
+  c <- getFreshClass @s
+  emitFor @s (selFrom c) m
+  return c
 
-type Has :: EffectRow -> EffectRow -> (EffectRow -> Effect) -> Constraint
-type Has s r f = (Member (f s) r, r ~ (f s : s))
+combinator
+  :: forall s r a . (RecEff s r CSS, SimpleSelectorFrom a)
+  => SOp -> a -> Sem r () -> Sem r ()
+combinator op slike m = do
+  selector <- getSelector @s
+  emitFor (Combined op selector (ssFrom slike)) m
 
--- * Helpers
+-- | Helper to make at rules.
+atRule
+  :: forall s r . RecEff s r CSS
+  => TS.Text -> TS.Text -> Sem r () -> Sem r ()
+atRule ruleName rule m = do
+  s <- getSelector @s
+  rules <- execDsl m <&> fst
+  emitRules @s $ pure $ AtRule ruleName rule rules
 
-execDsl_
-  :: forall s r a . Has s r CSS
-  => Sem (CSS s : s) a -> Sem (CSS s : s) OuterRules
-execDsl_ m = execDsl m <&> fst
+-- | Helper for CSS.TH. Run @m@ for selector modified by @mod@.
+combine
+  :: forall s r . RecEff s r CSS
+  => (Selector -> Selector) -> Sem r () -> Sem r ()
+combine mod m = getSelector @s >>= mod ^ flip emitFor m
+
+cssRule
+  :: forall s r a
+   . (RecEff s r CSS, SelectorFrom a)
+  => a -> Sem r () -> Sem r ()
+cssRule slike = emitFor (selFrom slike)
+
+-- * Compat
+
+type CSSF = forall s r . RecEff s r CSS => Sem r () -> Sem r ()
+type CSSM = forall s r . RecEff s r CSS => Sem r ()
+type PolyProp = forall r . Member Prop r => Sem r ()
