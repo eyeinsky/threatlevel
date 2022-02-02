132262ed06255c24a0f5c27bc16c19ffa16ba652
Markus Läll <markus.l2ll@gmail.com>
2022-02-02 18:19:03 +0200

    Add module Common.Polysemy

--- threatlevel-common/Common/Polysemy.hs
+++ threatlevel-common/Common/Polysemy.hs
@@ -1,7 +1,11 @@
-module Common.Polysemy where
+module Common.Polysemy
+  ( module Common.Polysemy
+  , module Export
+  ) where
 
 import Data.Kind
-import Polysemy
+import Polysemy as Export
+import Polysemy.Internal as Export (Append)
 
 type RecEff :: EffectRow -> EffectRow -> (EffectRow -> Effect) -> Constraint
 type RecEff s r f = (Member (f s) r, r ~ (f s : s))
