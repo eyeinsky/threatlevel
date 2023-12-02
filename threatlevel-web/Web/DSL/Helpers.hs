7da6d5cdb39c17af71d193a93d91ba3eff5ecb6e
Markus Läll <markus.l2ll@gmail.com>
2023-12-02 16:33:32 +0200

    Make `styled` use a HTML class instead of id, deprecate `styleds`
    `styleds` used to return a HTML element with a class while `styled`
    used an id. Deprecate the id method and use classes only. Anyone who
    wants an id can do it via `cssId` anyway.

--- threatlevel-web/Web/DSL/Helpers.hs
+++ threatlevel-web/Web/DSL/Helpers.hs
@@ -5,11 +5,12 @@ import CSS
 import XML
 
 
-styled :: (CSS m, Exclamatable a Id) => a -> CSSM -> m a
+styled :: (CSS m, Exclamatable a Class) => a -> m () -> m a
 styled elem rules = do
-  id <- cssId rules
-  return $ elem ! id
+  class_ <- css rules
+  return $ elem ! class_
 
+{-# DEPRECATED styleds "Use `styled` to get an element styled with a class, or `cssId`" #-}
 styleds :: (CSS m, Exclamatable a Class) => a -> CSSM -> m a
 styleds elem rules = do
   class_ <- css rules
