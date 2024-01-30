588dc5f523d86651b459811a8c05fedd1e9a2d6a
Markus Läll <markus.l2ll@gmail.com>
2024-01-30 18:23:09 +0200

    Remove lens from Server.Sessions

--- threatlevel-unsorted/Server/Sessions.hs
+++ threatlevel-unsorted/Server/Sessions.hs
@@ -1,8 +1,8 @@
 module Server.Sessions where
 
 import Common.Prelude hiding (map)
-import qualified Data.Text as TS
-import qualified Data.HashMap.Strict as HM
+import Data.Text qualified as TS
+import Data.HashMap.Strict qualified as HM
 import Data.Word (Word64)
 import Numeric
 import Control.Concurrent.MVar
@@ -16,22 +16,21 @@ data Store a = Store
   { storeMap :: MVar (HM.HashMap Id a)
   , storeGen :: IOGenM StdGen
   }
-makeFields ''Store
 
 createStore :: IO (Store a)
 createStore = Store <$> newMVar mempty <*> (newIOGenM =<< Random.initStdGen)
 
 update :: Id -> a -> Store a -> IO ()
-update id value store = modifyMVar_ (store^.map) $ pure . HM.insert id value
+update id value store = modifyMVar_ (storeMap store) $ pure . HM.insert id value
 
 new :: a -> Store a -> IO Id
 new value store = do
-  id <- genId $ store^.gen
+  id <- genId $ storeGen store
   update id value store
   return id
 
 get :: Id -> Store a -> IO (Maybe a)
-get id store = readMVar (store^.map) <&> HM.lookup id
+get id store = readMVar (storeMap store) <&> HM.lookup id
 
 -- * Helpers
 
@@ -40,4 +39,4 @@ genId gen = TS.concat . fmap sh <$> replicateM 4 (uniformM gen)
   where sh (bytes :: Word64) = TS.pack $ showHex bytes ""
 
 listTokens :: Store a -> IO [Id]
-listTokens store = readMVar (store^.map) <&> HM.keys
+listTokens store = readMVar (storeMap store) <&> HM.keys
